597baed6a43be16560e5b05e692c548ba63ebd0a
alex.han <alex.han@goertek.com>
2018-09-14 18:24:25 +0800

    by zoulb

--- src/adRecog.py
+++ src/adRecog.py
@@ -1,6 +1,8 @@
 from keras.layers import Dense,Conv2D,Dropout
 import cv2 as cv
 path='/home/li/zoulb/datasets/datasets/train/'
+path_yolo='/home/li/zoulb/datasets/yolo_datasets/train/'
+
 
 def showimage(im_name,points):
     im=cv.imread(path+im_name)
@@ -9,11 +11,16 @@ def showimage(im_name,points):
     cv.imshow('im',im)
     cv.waitKey(1000)
 
-def yoloFormatC(file):
+def yoloFormatC(src_f,obj_f):
     print ('begin convert format')
-    f = open(file)
+    f = open(src_f)
+    fw= open(obj_f,'w')
     width=416
     height=416
+    scalar_x=0.0
+    scalar_y=0.0
+    yolo_path=''
+
 
     lines = f.readlines()
     pre = '';
@@ -25,29 +32,48 @@ def yoloFormatC(file):
     for str in lines:
         #print(str)
         strs=str.split()
-        if(pre_pic==strs[0]):
-            tmp=tmp+' '+strs[2]+','+strs[3]+','+strs[4]+','+strs[5]+','+strs[1]
-            cv.rectangle(im, (int(strs[2]), int(strs[3])), (int(strs[4]), int(strs[5])), (255, 0, 0))
-        else:
-            if len(tmp)>10:
-                #print(tmp)
-                results.append(tmp+'\r')
-                tmp=''
-                cv.imshow('im', im)
-                cv.waitKey(1000)
-            tmp=path+strs[0]+' '+strs[2]+','+strs[3]+','+strs[4]+','+strs[5]+','+strs[1]
-            pre_pic=path+strs[0]
-            im = cv.imread(pre_pic)
-            print(im.shape)
-            scalar_x=width/im.shape[1]
-            scalar_y=height/im.shape[0]
-            print (scalar_x,scalar_y)
-            res = cv.resize(im, (width,height), interpolation=cv.INTER_CUBIC)
-            cv.rectangle(res, (int(int(strs[2])*scalar_x), int(int(strs[3])*scalar_y)), (int(int(strs[4])*scalar_x), int(int(strs[5])*scalar_y)), (255, 0, 0))
-            cv.imshow('tmp',res)
+        if len(strs)>3:
+            if(pre_pic==strs[0]):
 
-            cv.rectangle(im, (int(strs[2]),int(strs[3])),(int(strs[4]),int(strs[5])), (255, 0, 0))
+                x_min = int(int(strs[2]) * scalar_x)
+                y_min = int(int(strs[3]) * scalar_y)
+                x_max = int(int(strs[4]) * scalar_x)
+                y_max = int(int(strs[5]) * scalar_y)
 
+                frame_str = '{},{},{},{},{}'.format(x_min, y_min, x_max, y_max, strs[1])
+                tmp=tmp+' '+frame_str
+                cv.rectangle(im, (x_min, y_min), (x_max,y_max), (255, 0, 0))
+            else:
+                if len(tmp)>10:
+                    scalar_x = 0.0
+                    scalar_y = 0.0
+                    results.append(tmp+'\n')
+                    print(tmp)
+                    #cv.imwrite(yolo_path,im)
+                    tmp=''
+                    #cv.imshow('im', im)
+                    #cv.waitKey(1000)
+#
+                pre_pic=strs[0]
+                im = cv.imread(path+strs[0])
+                yolo_path=path_yolo+strs[0]
+                scalar_x = width / im.shape[1]
+                scalar_y = height / im.shape[0]
+
+                x_min = int(int(strs[2]) * scalar_x)
+                y_min = int(int(strs[3]) * scalar_y)
+                x_max = int(int(strs[4]) * scalar_x)
+                y_max = int(int(strs[5]) * scalar_y)
+
+                frame_str = '{},{},{},{},{}'.format(x_min, y_min, x_max, y_max, strs[1])
+                tmp = path_yolo + strs[0] + ' ' + frame_str
+                print(im.shape)
+                im = cv.resize(im, (width, height), interpolation=cv.INTER_CUBIC)
+                cv.rectangle(im, (x_min, y_min), (x_max, y_max), (255, 0, 0))
+
+    fw.writelines(results)
+    fw.close()
+    f.close()
     return results
 
 
@@ -69,17 +95,9 @@ def loadDatas(file,isshow):
     return pics
 
 
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
     #pics=loadDatas('/home/li/zoulb/datasets/datasets/train.txt',True)
-    pics = yoloFormatC('/home/li/zoulb/datasets/datasets/train.txt')
+    pics = yoloFormatC('/home/li/zoulb/datasets/datasets/train.txt','/home/li/zoulb/datasets/datasets/for_yolo.txt')
     for info in pics:
         print (info)
 
